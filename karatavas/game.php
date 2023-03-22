2ea114abba9eef3eabce2ec9abc13c0096c8a1fd
aivis-vigo <120174645+aivis-vigo@users.noreply.github.com>
2023-03-22 15:32:44 +0200

    Karatavas

---
+++ karatavas/game.php
@@ -0,0 +1,50 @@
+<?php
+
+echo "Welcome to Word Guessing Game!". PHP_EOL;
+echo "------------------------------";
+
+$words = [/**'balloon', 'weapon', 'church', 'market', 'spring',*/ 'banana'];
+$choice = array_rand($words);
+
+$gameWord = $words[$choice];
+$letters = str_split($gameWord);
+$answer = str_split(str_repeat("_", strlen($gameWord)));
+var_dump($gameWord);
+
+$guesses = 0;
+$maxGuesses = 3;
+
+while ($guesses < $maxGuesses)
+{
+    echo "Your word: ";
+    echo join("", $answer);
+
+    echo PHP_EOL;
+    $userInput = readline('Enter letter: ');
+    echo PHP_EOL;
+
+    $index = array_search($userInput, $letters);
+
+    if ($index > -1)
+    {
+        $answer[$index] = $userInput;
+        $letters[$index] = "-";
+
+        if ($gameWord == join("", $answer))
+        {
+            echo "------------------------------" . PHP_EOL;
+            echo "Word was " . $gameWord . "." . PHP_EOL;
+            echo "You win!" . PHP_EOL;
+            break;
+        }
+    } else
+    {
+        $guesses++;
+    }
+}
+
+if (!($gameWord == join("", $answer)))
+{
+    echo "------------------------------" . PHP_EOL;
+    echo "You lose!";
+}
