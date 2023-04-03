9f1f955dd0d6496ce3a652a4c6f798877677bb2d
aivis-vigo <120174645+aivis-vigo@users.noreply.github.com>
2023-04-03 15:42:18 +0300

    Delete arithmetic-operators directory

--- arithmetic-operators/exercise-2.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-function oddOrEven($num): string
-{
-    return ($num & 0 != 2) ? "$num is odd!" . PHP_EOL . "Bye!" : "$num is even!" . PHP_EOL . "Bye!";
-
-}
-
-echo oddOrEven(6);
